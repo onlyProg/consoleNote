9a3ea820d627cd65c14af288e57a2e7d0267cd78
onlyProg <onlyCyberLife@gmail.com>
2024-06-07 02:51:05 +0500

    Added functions for loading and unloading note data (load_data, unload_data).

--- src/main.c
+++ src/main.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define FILE_NAME ".note.txt"
+#define SIZE_LINE 1024
 
 /*
  * Getting the path to a file in the user's home directory.
@@ -43,10 +45,84 @@ int check_file(const char* path_to_file){
 	return create_file(path_to_file);
 }
 
+/*
+ * Wrapper over file opening. Opens files and terminates the program
+ * if it fails to open the file. Returns FILE on success.
+ */
+FILE* open_file(const char* path_to_file, const char* mode){
+	FILE* file;
+
+	file = fopen(path_to_file, mode);
+	if (file == NULL){
+		perror("open file");
+		exit(errno);
+	}
+
+	return file;
+}
+
+ssize_t count_lines(const char* path_to_file){
+	ssize_t count = 0;
+	char ch = 0;
+	FILE* file;
+
+	file = open_file(path_to_file, "r");
+
+	while ((ch = fgetc(file)) != EOF){
+		if (ch == '\n')
+			count++;
+	}
+
+	fclose(file);
+	return count;
+}
+
+/*
+ * Loading data into the passed array. If successful, returns an
+ * array; if unsuccessful, exits the program with an error code.
+ */
+char** load_data(const char* path_to_file, ssize_t* count_line){
+	char** array = NULL;
+	FILE* file = NULL;
+	ssize_t index = 0;
+
+	(*count_line) = count_lines(path_to_file);
+
+	file = open_file(path_to_file, "r");
+
+	array = malloc((*count_line) * sizeof(char*));
+	for (ssize_t i = 0; i < (*count_line); i++)
+		array[i] = malloc(SIZE_LINE * sizeof(char));
+	
+	while (fgets(array[index++], SIZE_LINE, file) != NULL);
+
+	fclose(file);
+	return array;
+}
+
+/*
+ * Uploading data back to a file and freeing up memory.
+ */
+void unload_data(const char* path_to_file, char*** array,
+		const ssize_t count_line){
+	FILE* file;
+
+	file = open_file(path_to_file, "w");
+
+	for (ssize_t i = 0; i < count_line; i++){
+		fputs((*array)[i], file);
+		free((*array)[i]);
+	}
+
+	fclose(file);
+}
+
 
 
 int main(void){
 	const char* path_to_file = NULL;
+	char** notes = NULL;
+	ssize_t count_line = 0;
 
 	// Working with a notes file
 	path_to_file = get_path_to_file(FILE_NAME);
@@ -55,6 +131,9 @@ int main(void){
 		exit(1);
 	}
 
+	notes = load_data(path_to_file, &count_line);
+
+	unload_data(path_to_file, &notes, count_line);
 	return 0;
 }
 
